c3cc7211b89ab8f435dd46ab0e497be28c307323
SanthoshMoolemane <84221152+SanthoshMoolemane@users.noreply.github.com>
2022-09-24 23:20:39 +0530

    Updates

---
+++ precache-manifest.42f28b2197b344b3f23bfa76703b3979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4fcba18fa4cdff82f6ecef71199f06",
+    "url": "/blog-app/index.html"
+  },
+  {
+    "revision": "c025907b6cdf2975e11e",
+    "url": "/blog-app/static/css/main.0a0dface.chunk.css"
+  },
+  {
+    "revision": "0c8c55336bccd1e5ce8b",
+    "url": "/blog-app/static/js/2.ef383bb6.chunk.js"
+  },
+  {
+    "revision": "c025907b6cdf2975e11e",
+    "url": "/blog-app/static/js/main.4b43b9d9.chunk.js"
+  },
+  {
+    "revision": "07581233709cc84b8f2f",
+    "url": "/blog-app/static/js/runtime~main.7e17012d.js"
+  }
+]);
